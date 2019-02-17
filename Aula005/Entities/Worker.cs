f0e9211b79d19a6e5d05cb50d47114cb75f29ade
Luciano <lucianotetsu@gmail.com>
2019-02-17 12:49:55 -0300

    Adicionando a classe Worker

---
+++ Aula005/Entities/Worker.cs
@@ -0,0 +1,24 @@
+﻿using Aula005.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula005.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public List<HourContract> 
+
+        public void AddContract (HourContract contract)
+        {
+
+           
+        }
+    }
+}
