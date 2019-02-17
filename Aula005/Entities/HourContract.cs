8bb3ef63e0284290a6e967d9fc2222792fca9248
Luciano <lucianotetsu@gmail.com>
2019-02-17 12:04:01 -0300

    Adicionando a classe HourContract

---
+++ Aula005/Entities/HourContract.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula005.Entities
+{
+    class HourContract
+    {
+        public DateTime Data { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        public HourContract(DateTime data, double valuePerHour, int hours)
+        {
+            Data = data;
+            ValuePerHour = valuePerHour;
+            Hours = hours;
+        }
+
+        public double Value() {
+
+            return ValuePerHour * Hours;
+        }
+    }
+}
